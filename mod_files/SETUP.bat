a34f36e39b9101e807fbb28acc80a7c93cb09d39
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2021-11-06 18:11:20 -0400

    Adapt mod setup script.

--- mod_files/SETUP.bat
+++ mod_files/SETUP.bat
@@ -3,6 +3,11 @@
 @echo off
 
 echo Setup starting...
+
+:: Copy mp3 sound files to media folder.
+xcopy /f /i /y /s sound\mp3\*.mp3 media\invasion\sound\mp3\
+xcopy /f /i /y /s sound\outro\*.mp3 media\invasion\sound\outro\
+
 echo Setup successfully completed
 
 endlocal
